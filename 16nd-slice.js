27d5507deaf7642ad565c2202970502d8c58d90d
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2024-05-04 02:03:14 +0430

    create slice method

---
+++ 16nd-slice.js
@@ -0,0 +1,13 @@
+// function sliceMethod(arr,startIndex,endIndex) {
+//     let newArr = [];
+//     for(let i = 0; i < arr.length; i++){
+//        if(startIndex){
+//          for(let j = 0; j < arr.length - startIndex; j++){
+            
+//          }
+//        } 
+//     }
+//     return newArr
+// }
+
+// console.log(sliceMethod([1,2,3,4,5],1))
